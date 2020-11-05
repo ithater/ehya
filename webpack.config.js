9169eaffd5b1fa6ed00a95856e71fd89c7cd2f2f
ithater <ithater.iwnl@gmail.com>
2020-11-05 17:02:45 +0300

    cr: banner

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ const babelOptions = (preset, plugins) => {
 };
 
 const PATHS = {
-  dist: path.resolve(__dirname, 'dist'),
+  dist: path.resolve(__dirname, 'docs'),
   src: path.resolve(__dirname, 'src'),
   assets: '/assets',
 };
