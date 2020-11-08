b166e7c1a65cf2fe6a36ec394ae568fe081b362d
ithater <ithater.iwnl@gmail.com>
2020-11-08 11:50:57 +0300

    fix: header

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,13 +1,34 @@
 const header = () => {
-  const burgerButton = document.querySelector('.button-burger');
-  const menu = document.querySelector('.header-menu');
-
-  const toggleMenu = () => {
-    menu.classList.toggle('header-menu--active');
-  };
+	const burgerButton = document.querySelector('.button-burger');
+	const closeBtn = document.querySelector('.header-menu__button-close');
+	const menu = document.querySelector('.header-menu');
 
 
-  burgerButton.addEventListener('click', toggleMenu);
+	const openMenu = () =>  menu.classList.add('header-menu--active');
+	const closeMenu = () =>  menu.classList.remove('header-menu--active');
+	let isOpen = false;
+
+	document.addEventListener('click', evt => {
+		const target = evt.target;
+
+		switch (target) {
+			case burgerButton:
+				isOpen = true;
+				openMenu();
+				break;
+			case closeBtn:
+				isOpen = false;
+				closeMenu();
+				break;
+			default:
+				if (isOpen) {
+					isOpen = false;
+					closeMenu();
+				}
+		}
+
+
+	});
 };
 
 export default header;
