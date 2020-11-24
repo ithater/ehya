1b080b166ae029baf01997804ec2272fe3635ffd
ithater <ithater.iwnl@gmail.com>
2020-11-24 12:31:04 +0300

    cr: validation

--- src/blocks/subscribe/subscribe.js
+++ src/blocks/subscribe/subscribe.js
@@ -1,8 +1,33 @@
 import SendForm from '@/lib/SendForm';
+import Validate from '@/lib/Validate';
 
 const subscribe = () => {
+	const validation = new Validate({
+		formId: 'subscribe-form',
+		inputs: [
+			{
+				inputId: 'subscribe-form-email',
+				messages: {
+					require: 'Данное поле должно быть заполнено',
+					func: 'Введите корректный email адресс',
+				},
+				validationFunc(value) {
+					// eslint-disable-next-line no-useless-escape
+					const isEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/.test(value);
+					return isEmail;
+				}
+			}
+		],
+		errorClass: 'invalid'
+	});
+
+
+
 	const submit = function(evt) {
 		evt.preventDefault();
+
+		if (validation.isValid === false) return;
+
 		const formData = new FormData(this.form);
 		const body = {};
 		formData.forEach((val, key) => body[key] = val);
@@ -14,6 +39,7 @@ const subscribe = () => {
 		formId: 'subscribe-form',
 		submit,
 	});
+
 
 	sendForm.init();
 };
---
+++ src/lib/Validate.js
@@ -0,0 +1,107 @@
+export default class Validate {
+	constructor({
+		inputs,
+		formId,
+		errorClass
+	}) {
+		this.inputs = inputs;
+		this.$form = document.getElementById(formId);
+		this.errorClass = errorClass;
+		this.init();
+	}
+
+	init() {
+		this.inputs = this.inputs.map(inputInfo => {
+			const $error = document.createElement('label');
+			$error.setAttribute('for', inputInfo.inputId);
+			$error.className = this.errorClass;
+
+			return {
+				input: document.getElementById(inputInfo.inputId),
+				inputId: inputInfo.inputId,
+				messages: inputInfo.messages,
+				validationFunc: inputInfo.validationFunc,
+				isValid: false,
+				$error,
+				isDisplayed: false,
+			};
+		});
+
+		const _this = this;
+		const _onSubmitHandler = evt => _this.onSubmitHandler(evt, _onSubmitHandler);
+
+		this.$form.addEventListener('submit', _onSubmitHandler);
+	}
+
+	get isValid() {
+		const isValid = this.inputs.every(input => input.isValid);
+		return isValid;
+	}
+
+	validate() {
+		this.inputs.forEach(inputData => {
+			const value = inputData.input.value;
+
+			// если пустая
+			if (value.trim() === '') {
+				inputData.isValid = false;
+				this.showRequireMessage(inputData);
+				return;
+			}
+
+			const isValid = inputData.validationFunc(value);
+			// если не прошла валидацию через функцию
+			if (!isValid) {
+				inputData.isValid = false;
+				this.showFuncMessage(inputData);
+				return;
+			}
+
+			// если всё прошла
+			this.hideMessage(inputData);
+			inputData.isValid = true;
+		});
+	}
+
+	hideMessage(inputData) {
+		console.log('inputData: ', inputData);
+		if (inputData.isDisplayed) {
+			inputData.$error.remove();
+			inputData.isDisplayed = false;
+		}
+	}
+
+	showRequireMessage(inputData) {
+		console.log('inputData: ', inputData);
+		if (inputData.isDisplayed) {
+			inputData.$error.textContent = inputData.messages.require;
+		} else {
+			inputData.$error.textContent = inputData.messages.require;
+			inputData.input.insertAdjacentElement('afterend', inputData.$error);
+			inputData.isDisplayed = true;
+		}
+	}
+
+	showFuncMessage(inputData) {
+		console.log('inputData: ', inputData);
+		if (inputData.isDisplayed) {
+			inputData.$error.textContent = inputData.messages.func;
+		} else {
+			inputData.$error.textContent = inputData.messages.func;
+			inputData.input.insertAdjacentElement('afterend', inputData.$error);
+			inputData.isDisplayed = true;
+		}
+	}
+
+	onSubmitHandler(evt, _onSubmitHandler) {
+		evt.preventDefault();
+		this.validate();
+		this.$form.addEventListener('input', this.onInputHandler.bind(this));
+		this.$form.removeEventListener('submit', _onSubmitHandler);
+	}
+
+	onInputHandler(evt) {
+		evt.preventDefault();
+		this.validate();
+	}
+}
